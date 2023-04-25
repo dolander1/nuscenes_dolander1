8fbb949a092344bd651b3963fc93b321be5d0695
dolander1 <dolander@volvocars.com>
2023-04-25 11:04:35 +0000

    saveData for ADE

---
+++ python-sdk/tutorials/trainFL.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+# GPT with covernet input
+
+import importlib
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+import pickle
+import numpy as np
+import os
+
+from nuscenes.prediction.models.backbone import ResNetBackbone
+
+from utilsHannes import CoverNetNoRelu
+from utilsHannes import ConstantLatticeLoss
+from utilsHannes import mean_pointwise_l2_distance
+import utilsHannes as utilsH
+importlib.reload(utilsH)
+
+#################################################################################################################################
+# Define your custom dataset class that inherits from torch.utils.data.Dataset
+class NuscenesDataset(Dataset):
+    def __init__(self, image_data, agent_state_data, ground_truth_data):
+        self.image_data = image_data
+        self.agent_state_data = agent_state_data
+        self.ground_truth_data = ground_truth_data
+        
+    def __len__(self):
+        return len(self.image_data)
+    
+    def __getitem__(self, index):
+        image_data_item = self.image_data[index]
+        agent_state_data_item = self.agent_state_data[index]
+        ground_truth_data_item = self.ground_truth_data[index]
+        
+        return image_data_item, agent_state_data_item, ground_truth_data_item
+
+################################################################################################################################################
+# Load data
+version = "v1.0-trainval" # v1.0-mini, v1.0-trainval
+seconds_of_history_used = 2.0 # 2.0
+sequences_per_instance = "one_sequences_per_instance" # one_sequences_per_instance, non_overlapping_sequences_per_instance, all_sequences_per_instance, 
+
+train_img_tensor_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/train_img_tensor_list.pt")
+train_agent_state_vector_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/train_agent_state_vector_list.pt")
+train_future_xy_local_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/train_future_xy_local_list.pt")
+
+val_img_tensor_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/val_img_tensor_list.pt")
+val_agent_state_vector_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/val_agent_state_vector_list.pt")
+val_future_xy_local_list = torch.load(f"dataLists/{version}/{sequences_per_instance}/{seconds_of_history_used}/val_future_xy_local_list.pt")
+
+# Squeeze for correct dimensions
+for i, train_img_tensor in enumerate(train_img_tensor_list):
+    train_img_tensor_list[i] = torch.squeeze(train_img_tensor, dim=0)
+    train_agent_state_vector_list[i] = torch.squeeze(train_agent_state_vector_list[i], dim=0)
+    
+for j, val_img_tensor in enumerate(val_img_tensor_list):
+    val_img_tensor_list[j] = torch.squeeze(val_img_tensor, dim=0)
+    val_agent_state_vector_list[j] = torch.squeeze(val_agent_state_vector_list[j], dim=0)
+
+
+################################################################################################################################################
+# For testing
+train_short_size = 10
+short_train_img_tensor_list = train_img_tensor_list[:train_short_size]
+short_train_agent_state_vector_list = train_agent_state_vector_list[:train_short_size]
+short_train_future_xy_local_list = train_future_xy_local_list[:train_short_size]
+val_short_size = 2
+short_val_img_tensor_list = val_img_tensor_list[:val_short_size]
+short_val_agent_state_vector_list = val_agent_state_vector_list[:val_short_size]
+short_val_future_xy_local_list = val_future_xy_local_list[:val_short_size]
+
+
+# Prints
+train_num_datapoints = len(train_img_tensor_list)
+short_train_num_datapoints = len(short_train_img_tensor_list)
+print(f"train_num_datapoints = {train_num_datapoints}")
+print(f"train_num_datapoints short = {short_train_num_datapoints}")
+val_num_datapoints = len(val_img_tensor_list)
+short_val_num_datapoints = len(short_val_img_tensor_list)
+print(f"val_num_datapoints = {val_num_datapoints}")
+print(f"val_num_datapoints short = {short_val_num_datapoints}")
+
+
+# Variables
+batch_size = 16
+shuffle = True # Set to True if you want to shuffle the data in the dataloader
+num_modes = 64 # 2206, 415, 64 (match with eps_traj_set)
+eps_traj_set = 8 # 2, 4, 8 (match with num_modes)
+learning_rate = 1e-4 # From Covernet paper: fixed learning rate of 1e−4
+num_epochs = 2
+accum_iter = 4 # batch accumulation parameter, multiplies batch_size
+
+# Define datasets
+train_shortDataset = NuscenesDataset(short_train_img_tensor_list, short_train_agent_state_vector_list, short_train_future_xy_local_list)
+val_shortDataset = NuscenesDataset(short_val_img_tensor_list, short_val_agent_state_vector_list, short_val_future_xy_local_list)
+
+# Instantiate dataloaders
+train_shortDataloader = DataLoader(train_shortDataset, batch_size=batch_size, shuffle=shuffle)
+val_shortDataloader = DataLoader(val_shortDataset, batch_size=batch_size, shuffle=shuffle)
+
+# Initialize the CoverNet model
+backbone = ResNetBackbone('resnet50') 
+covernet = CoverNetNoRelu(backbone, num_modes)
+
+# Lattice and similarity function
+with open(f'data/sets/nuscenes-prediction-challenge-trajectory-sets/epsilon_{eps_traj_set}.pkl', 'rb') as f:
+    lattice = np.array(pickle.load(f))
+similarity_function = mean_pointwise_l2_distance
+
+# Define your loss function and optimizer
+loss_function = ConstantLatticeLoss(lattice, similarity_function)
+# optimizer = optim.Adam(covernet.parameters(), lr=learning_rate) 
+optimizer = optim.SGD(covernet.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4) # from author https://github.com/nutonomy/nuscenes-devkit/issues/578
+
+
+# Move the model to GPU if available
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+covernet.to(device)
+lattice = torch.Tensor(lattice).to(device)
+
+
+# Training starts
+print("\nTraining starts:")
+
+
+# Open a file in append mode (will create a new file or append to an existing one)
+file_path = f"tmpResults/results_epochs={num_epochs}_seconds_of_history_used={seconds_of_history_used}_{sequences_per_instance}"
+results_string = ""
+logits_file = f'{file_path}_logits.npy'
+gt_traj_file = f'{file_path}_ground_truth_trajectory.npy'
+
+
+# Training and validation loop
+for epoch in range(num_epochs):
+    
+    # TRAINING
+    covernet.train()
+    train_epochLoss = 0
+    train_total = 0
+    train_correct = 0
+    for train_batchCount, train_batch in enumerate(train_shortDataloader):
+
+        # Get train_batch data
+        image_tensor, agent_state_vector, ground_truth_trajectory = train_batch
+
+        # Send to device
+        image_tensor = image_tensor.to(device)
+        agent_state_vector = agent_state_vector.to(device)
+        ground_truth_trajectory = ground_truth_trajectory.to(device)
+        
+        # Forward pass
+        logits = covernet(image_tensor, agent_state_vector)
+
+        # Compute loss
+        loss = loss_function(logits, ground_truth_trajectory)
+        train_epochLoss += loss.item()
+
+#         # Backward pass
+#         loss.backward()
+#         optimizer.step()
+        
+#         # Zero the gradients
+#         optimizer.zero_grad()
+         
+        loss = loss / accum_iter
+        
+        # Backward pass
+        loss.backward()
+        
+        # Update weights
+        if ((train_batchCount + 1) % accum_iter == 0) or (train_batchCount + 1 == len(train_shortDataloader)):
+            optimizer.step()
+            optimizer.zero_grad()
+        
+        # Compute accuracy
+        for logit, ground_truth in zip(logits, ground_truth_trajectory):
+            _, predicted = torch.max(logit, 0)
+            closest_lattice_trajectory = similarity_function(lattice, ground_truth)
+            train_total += 1
+            train_correct += (predicted == closest_lattice_trajectory)#.sum().item()
+#             print("Train Predicted lattice trajectory:", predicted.item())
+#             print("Train Actual closest lattice trajectory:", closest_lattice_trajectory.item())
+
+        # Print loss for this train_batch
+        # print(f"train_batch [{train_batchCount+1}/{int(short_train_num_datapoints/batch_size)+1}], Batch Loss: {loss.item():.4f}")
+     
+    
+    # VALIDATION
+    covernet.eval()
+    val_epochLoss = 0
+    val_total = 0
+    val_correct = 0
+    logits_list = [] # create an empty list to store logits
+    gt_traj_list = [] # create an empty list to store ground_truth_trajectory
+    with torch.no_grad():
+        for val_batchCount, val_batch in enumerate(val_shortDataloader):
+
+            # Get val_batch data
+            image_tensor, agent_state_vector, ground_truth_trajectory = val_batch
+
+            # Send to device
+            image_tensor = image_tensor.to(device)
+            agent_state_vector = agent_state_vector.to(device)
+            ground_truth_trajectory = ground_truth_trajectory.to(device)
+
+            # Forward pass
+            logits = covernet(image_tensor, agent_state_vector)
+
+            # Compute loss
+            loss = loss_function(logits, ground_truth_trajectory)
+            val_epochLoss += loss.item()
+            
+            val_total += ground_truth_trajectory.size(0)
+            _, predicted = torch.max(logits, 1) 
+#             print(f"ground_truth_trajectory.shape = {ground_truth_trajectory.shape}") # [batch_size, 12, 2]
+#             print(f"predicted.shape = {predicted.shape}") # [batch_size]
+#             print(f"logits.shape = {logits.shape}") # [batch_size, num_modes]
+            
+            for index, ground_truth in enumerate(ground_truth_trajectory):
+                closest_lattice_trajectory = similarity_function(lattice, ground_truth)
+                val_correct += (predicted[index] == closest_lattice_trajectory)#.sum().item()
+#                 print(f"closest_lattice_trajectory index = {closest_lattice_trajectory}") # 1
+#                 print(f"Predicted lattice trajectory index = {predicted[index].item()}") # 1
+#                 print(f"ground_truth = {ground_truth}") # [2, 12]
+#                 print(f"predicted = {lattice[predicted[index].item()]}") # [2, 12]
+#                 print("----------------------------------------------------------------")
+                
+            
+            # Create lists of saved data
+            logits_list.append(logits.cpu().numpy())
+            gt_traj_list.append(ground_truth_trajectory.cpu().numpy())
+
+    # Save logits and ground_truth_trajectory in separate files
+    if epoch == 0:
+        # Concatenate the lists to create numpy arrays
+        logits_array = np.concatenate(logits_list, axis=0)
+        gt_traj_array = np.concatenate(gt_traj_list, axis=0)
+        # save numpy arrays in files
+        np.save(logits_file, logits_array)
+        np.save(gt_traj_file, gt_traj_array)
+    else:
+        logits_array = np.load(logits_file)
+        gt_traj_array = np.load(gt_traj_file)
+        # Concatenate the lists to create numpy arrays
+        logits_array = np.concatenate([logits_array] + logits_list, axis=0)
+        gt_traj_array = np.concatenate([gt_traj_array] + gt_traj_list, axis=0)
+        # save numpy arrays in files
+        np.save(logits_file, logits_array)
+        np.save(gt_traj_file, gt_traj_array)
+  
+
+    # Print losses for this epoch
+    thisResult = f"Epoch [{epoch+1}/{num_epochs}]: Training loss: {train_epochLoss/train_total:.5f} | Validation loss: {val_epochLoss/val_total:.5f} || Training accuracy: {train_correct/train_total*100:.1f} % | Validation accuracy: {val_correct/val_total*100:.1f} %\n"
+    with open(f'{file_path}.txt', "a") as file:
+        file.write(thisResult)  # Append the text to the file
+    print(thisResult)
+    
+    # Save weights every epoch
+    torch.save(covernet.state_dict(), f'{file_path}_weights.pth')
+
+
+# Training complete
+print("Training complete!")
+
+
